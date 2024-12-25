881d5f72ba313fa24f071aad4c4999938a1b2d3b
rama-27 <ramarajumantena231@gmail.com>
2024-12-25 16:54:44 +0530

    simpleJavaApplication

---
+++ src/main/java/dev/rama27/Main.java
@@ -0,0 +1,9 @@
+package dev.rama27;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String...args) {
+        System.out.println("Hello world!"+ List.of(args));
+    }
+}
