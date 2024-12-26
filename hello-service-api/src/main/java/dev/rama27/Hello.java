1e1e45a91e9f88799a2ab71c60ab47e33c958ec6
rama-27 <ramarajumantena231@gmail.com>
2024-12-26 16:46:33 +0530

    stuff

---
+++ hello-service-api/src/main/java/dev/rama27/Hello.java
@@ -0,0 +1,5 @@
+package dev.rama27;
+
+public interface Hello {
+    void sayHello(String name);
+}
---
+++ hello-service-stdout/src/main/java/dev/rama27/StdOutHello.java
@@ -0,0 +1,15 @@
+package dev.rama27;
+
+public class StdOutHello implements Hello{
+    private final String greeting;
+    private  StdOutHello(String greeting) {
+        this.greeting = greeting;
+    }
+    @Override
+    public void sayHello(String name) {
+        System.out.println(greeting + " " + name);
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
