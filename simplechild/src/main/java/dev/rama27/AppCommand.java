71b6b0531a6fe9c156d2489068a8553e75efa6df
rama-27 <ramarajumantena231@gmail.com>
2024-12-26 12:50:54 +0530

    PEEEK

--- simplechild/src/main/java/dev/rama27/AppCommand.java
+++ simplechild/src/main/java/dev/rama27/AppCommand.java
@@ -1,4 +1,14 @@
 package dev.rama27;
 
-public class AppCommand {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AppCommand implements CommandLineRunner {
+    public void run(String ... args) throws Exception {
+        System.out.println("Component Says YO!!! "+ List.of(args));
+
+    }
 }
